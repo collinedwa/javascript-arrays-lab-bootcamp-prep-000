936c6ffbf1885f964e65ef3c565fff1c5820557c
Collin Edwards <collinedwards1579@gmail.com>
2018-01-19 21:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kittens){
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(kittens){
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function appendKitten(kittens){
+  return [...kittens, "Broom"]
+}
+
+function prependKitten(kittens){
+  kittens.slice(5)
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten(kittens){
+  kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(kittens){
+  kittens.slice(1)
+}
