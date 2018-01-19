d075a9e5c71f83daef83006a025bf34fe97b555d
Collin Edwards <collinedwards1579@gmail.com>
2018-01-19 21:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ function prependKitten(kittens){
 
 function removeLastKitten(kittens){
   kittens.slice(0, kittens.length - 1)
+  return kittens
 }
 
 function removeFirstKitten(kittens){
   kittens.slice(1)
+  return kittens
 }
